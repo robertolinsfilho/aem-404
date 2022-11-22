a92801e7404851b0cc6681d32134d987bce04d5f
Roberto Lins Filho <roberto.lins@compasso.com.br>
2022-11-22 14:18:57 -0300

    att

--- ui.frontend/src/components/MeuComponente/meuComponente.js
+++ ui.frontend/src/components/MeuComponente/meuComponente.js
@@ -3,11 +3,11 @@ import  './meuComponente.scss'
 
 
 
-const MeuComponente = () => {
+const MeuComponente = ({text,text2}) => {
   return(
     <div  class="have">
-      <h1   class="frase1"   >I have bad news for you </h1>
-      <h4  class="frase2">The page you are looking for might be removed or is temporarily unavailable</h4>
+      <h1   class="frase1"   >{text ? text : 'I have bad news for you'} </h1>
+      <h4  class="frase2">{text2 ? text2 : 'The page you are looking for might be removed or is temporarily unavailable'}</h4>
       <button class="button">BACK TO HOME</button>
     </div>
   );
